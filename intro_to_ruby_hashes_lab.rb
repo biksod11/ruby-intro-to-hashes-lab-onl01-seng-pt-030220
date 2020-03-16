7f268db837b55304ba629fa1b6c28288ec1ecbb4
Bikram Sodhi <biksodhi@gmail.com>
2020-03-16 00:17:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :names => {},
+      :names => {
+        :reading_railroad => {}
+      },
       :rent_in_dollars=> {}
     }
   }
